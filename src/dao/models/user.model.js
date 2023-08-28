9413970f11a545b1c63de2cc67de6a36354ef3ff
Luis Mera <luismera86@gmail.com>
2023-08-28 13:52:19 -0300

    Desafío con las vistas completado

--- src/dao/models/user.model.js
+++ src/dao/models/user.model.js
@@ -3,7 +3,10 @@ import { Schema, model } from "mongoose";
 const userSchema = new Schema({
   first_name: String,
   last_name: String,
-  email: String,
+  email: {
+    type: String,
+    unique: true, // El mail debe ser único
+  },
   age: Number,
   password: String,
 });
--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { productManagerDB } from "../dao/managers/mongoDBManagers/product.manager.js";
 import { cartManagerDB } from "../dao/managers/mongoDBManagers/cart.manager.js";
+import { userManager } from "../dao/managers/mongoDBManagers/user.manager.js";
 
 const routerViews = Router();
 
@@ -91,6 +92,118 @@ routerViews.get("/cart/:cid", async (req, res) => {
     if (!cart) return res.status(404).json({ msg: "Carrito no encontrado" });
 
     res.render("cart", { products: cart.products });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Vista de login
+routerViews.get("/login", async (req, res) => {
+  try {
+    res.render("login");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+routerViews.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    // Verificamos los datos ingresados
+    const user = await userManager.getUserByEmail(email);
+    if (!user || user.password !== password) return res.render("login", { error: "Usuario o contraseña incorrectos" });
+
+    const { first_name, last_name, age, email: emailUser } = user;
+    // Verificamos si el usuario es administrador le asignamos el rol de admin sino le asignamos el rol de user
+    if (email === "adminCoder@coder.com" || password === "adminCod3r123") {
+      req.session.user = {
+        first_name,
+        last_name,
+        age,
+        email: emailUser,
+        role: "admin",
+      };
+    } else {
+      req.session.user = {
+        first_name,
+        last_name,
+        age,
+        email: emailUser,
+        role: "user",
+      };
+    }
+
+    // Redireccionamos al perfil del usuario
+    return res.redirect("/profile");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Vista de registro
+routerViews.get("/register", async (req, res) => {
+  try {
+    res.render("register");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+routerViews.post("/register", async (req, res) => {
+  const { first_name, last_name, age, email, password } = req.body;
+  try {
+    // Verificamos si el usuario ya existe
+    const user = await userManager.getUserByEmail(email);
+    if (user) {
+      return res.render("register", { error: `El usuario con el mail ${email} ya existe` });
+    }
+
+    // Verificamos que ingreso todos los datos
+    if (!first_name || !last_name || !age || !email || !password) {
+      return res.render("register", { error: "Debe ingresar todos los datos" });
+    }
+
+    // Creamos el usuario
+    const newUser = await userManager.createUser({
+      first_name,
+      last_name,
+      email,
+      age,
+      password,
+    });
+
+    // Devolvemos el usuario creado
+    return res.redirect("/login");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Vista de perfil
+routerViews.get("/profile", async (req, res) => {
+  try {
+    const { user } = req.session;
+
+    // Si no hay usuario logueado redireccionamos al login
+    if (!user) return res.redirect("/login");
+
+    res.render("profile", { user });
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+// Cerrar sesión
+routerViews.get("/logout", async (req, res) => {
+  try {
+    // Destruimos la sesión
+    req.session.destroy((err) => {
+      if (err) {
+        return res.status(500).json({ error: "Error al cerrar sesión" });
+      }
+      // Redireccionamos al login
+      res.redirect("/login");
+    });
   } catch (error) {
     console.log(error);
   }
